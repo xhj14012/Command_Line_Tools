c0790f76c5d414844ff9f9e93967a54ba49f094d
徐浩杰 <xuhaojie@vtstar.net>
2018-05-24 10:34:47 +0800

    add ios

---
+++ iOS/enable_cydia_network/preinstall/preinstall.sh
@@ -0,0 +1,1 @@
+dpkg -i "/var/mobile/Documents/preinstall/mobilesubstrate_0.9.6301_iphoneos-arm.deb" "/var/mobile/Documents/preinstall/com.saurik.substrate.safemode_0.9.6001_iphoneos-arm.deb" "/var/mobile/Documents/preinstall/com.ioscreatix.conditionalwifi2_1.0.0_iphoneos-arm.deb"
---
+++ iOS/note.md
@@ -0,0 +1,6 @@
+only for cn mod iphones
+1. put files under "/var/mobile/Documents/" using Filza or some others
+2. enable bash tools and run preinstall.sh
+3. respring
+4. enable cydia network in setting-wifiusage
+5. cydia should work now
