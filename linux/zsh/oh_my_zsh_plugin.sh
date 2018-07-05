5da22eb10d93f3cc938aecfe7ae82029ead146f3
徐浩杰 <xuhaojie@vtstar.net>
2018-07-05 11:40:21 +0800

    update linux

---
+++ linux/zsh/oh_my_zsh_plugin.sh
@@ -0,0 +1,30 @@
+#part 1
+sudo apt-get update
+sudo apt-get install curl wget git zsh vim -y
+#  set zsh as default
+chsh -s `which zsh`
+#need reboot
+#sudo shutdown -r 0
+#part 2
+chsh -s /bin/zsh
+# install oh_my_zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# add "zsh-syntax-highlighting" plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# get key words
+cat ~/.oh-my-zsh/plugins/git/git.plugin.zsh
+# add "zsh-autosuggestions" plugin
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+# add "git-open" plugin
+git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
+vim ~/.zshrc
+# set plugins
+# "sublime z zsh-syntax-highlighting zsh-autosuggestions git-open themes"
+# set alias
+# alias go="git-open"
+# alias cp="cp -i"
+# alias brew="sudo apt"
+# export https_proxy=http://127.0.0.1:12333;export http_proxy=http://127.0.0.1:12333;export all_proxy=socks5://127.0.0.1:12333
+source ~/.zshrc
+# ZSH_THEME="random"
