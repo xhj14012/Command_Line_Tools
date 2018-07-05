4dbcd9de0d25d58efa9b665d613fd3aa0b5cabce
徐浩杰 <xuhaojie@vtstar.net>
2018-07-05 11:19:07 +0800

    update

--- windows/Win10Activation_legacy.bat
+++ windows/Win10Activation_legacy.bat
@@ -2,8 +2,8 @@
 
 ::set a0=example.com
 
-set a1=kms1.qinghuiss.com
-set a2=kms2.qinghuiss.com
+::set a1=kms1.qinghuiss.com
+::set a2=kms2.qinghuiss.com
 ::set a3=kms3.qinghuiss.com
 ::set a4=kms4.qinghuiss.com
 ::set a5=kms5.qinghuiss.com
