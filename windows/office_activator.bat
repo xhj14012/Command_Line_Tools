e06b2778bd6034f60ec5184953b6b34764944959
徐浩杰 <xuhaojie@vtstar.net>
2018-07-18 11:23:01 +0800

    add office_activator

---
+++ windows/office_activator.bat
@@ -0,0 +1,11 @@
+##ofiice kms active script
+#custom kms server
+cscript ospp.vbs /sethst:kms1.qinghuiss.com
+#active
+cscript ospp.vbs /act
+#check
+cscript"C:\Program Files\Microsoft Office\Office16\OSPP.VBS" /dstatus
+#downloads
+# ed2k://|file|SW_DVD5_Office_Professional_Plus_2016_64Bit_ChnSimp_MLF_X20-42426.ISO|1123452928|31087A00FF67D4F5B4CBF4AA07C3433B|/
+# ed2k://|file|SW_DVD5_Visio_Pro_2016_64Bit_ChnSimp_MLF_X20-42759.ISO|714913792|FC930AB97B366B3595FC2F28ABAC2A6F|/
+# ed2k://|file|SW_DVD5_Project_Pro_2016_64Bit_ChnSimp_MLF_X20-42676.ISO|647157760|0BBBF20CA3A5F61A819586ADCE6E4DCB|/
